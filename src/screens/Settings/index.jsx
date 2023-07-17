5ac8892822c21075d71e0a37cdb5e1d2ed4cf84f
Milit Bhatt <militbhatt@yahoo.com>
2023-07-17 12:08:12 -0400

    [#7] Added the Settings page to modify Notification settings.

--- src/screens/Settings/index.jsx
+++ src/screens/Settings/index.jsx
@@ -1,14 +1,97 @@
-import { View, Text } from 'react-native'
-
+import { View, TouchableOpacity, Switch, Modal } from 'react-native'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Button, Surface, Text, useTheme, Appbar } from 'react-native-paper'
+import React, { useState, useEffect } from 'react';
 // Common Components
 import hoc from '../../components/HOC'
 
-const SettingsScreen = () => {
+import styles from './styles'
+const SettingsScreen = ({ hideOption = false }) => {
+  const navigation = useNavigation();
+
+  const [notificationEnabled, setNotificationEnabled] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [soundEnabled, setSoundEnabled] = useState(true);
+
+  const handleNotificationToggle = () => {
+    setNotificationEnabled(previousState => !previousState);
+  };
+
+  const handleSoundToggle = () => {
+    setSoundEnabled(previousState => !previousState);
+  };
+
+  const handleOpenModal = () => {
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <View>
-      <Text>
-        SettingsScreen
-      </Text>
+    <View style={{ flex: 1, paddingLeft: 16, paddingRight: 16, width: "100%"}}>
+        <View style={styles.headerMain}>
+          <View>
+            {!hideOption &&
+              <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+                <MaterialCommunityIcons name="menu" size={28} color="black" />
+              </TouchableOpacity>
+            }
+          </View>
+          <View style={styles.header}>
+            <Text variant="headlineSmall" style={styles.name}>Settings</Text>
+          </View>
+      </View>
+      <View style={styles.settingRow}>
+        {/* <Text style={styles.settingLabel}>Enable Notification:</Text>
+        <Switch
+          value={notificationEnabled}
+          onValueChange={handleNotificationToggle}
+          thumbColor={notificationEnabled ? '#4e32bc' : '#f0f0f0'}
+          trackColor={{ false: '#0000', true: '#f0f0f0' }}
+        /> */}
+      </View>
+      <TouchableOpacity style={styles.settingRow} onPress={handleOpenModal}>
+        <MaterialCommunityIcons name="bell-outline" size={24} color="#4e32bc" />
+        <Text style={styles.settingsTitle}>Notifications</Text>
+      </TouchableOpacity>
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={handleCloseModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+          <View style={styles.modalHeader}>
+              <Text style={styles.modalTitle}>Notification Settings</Text>
+              <TouchableOpacity style={styles.modalCloseButton} onPress={handleCloseModal}>
+                <Text style={styles.modalCloseButtonText}>Close</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.notificationSettingRow}>
+              <Text style={styles.settingLabel}>Enable Notifications:</Text>
+              <Switch
+                value={notificationEnabled}
+                onValueChange={handleNotificationToggle}
+                thumbColor={notificationEnabled ? '#4e32bc' : '#f0f0f0'}
+                trackColor={{ false: '#f0f0f0', true: '#f0f0f0' }}
+              />
+            </View>
+            <View style={styles.notificationSettingRow}>
+              <Text style={styles.settingLabel}>Sound:</Text>
+              <Switch
+                value={soundEnabled}
+                onValueChange={handleSoundToggle}
+                thumbColor={soundEnabled ? '#4e32bc' : '#f0f0f0'}
+                trackColor={{ false: '#f0f0f0', true: '#f0f0f0' }}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
---
+++ src/screens/Settings/styles.js
@@ -0,0 +1,84 @@
+import { StyleSheet } from "react-native";
+import { useTheme } from "react-native-paper";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 5,
+        width: '100%',
+        // marginBottom: 20
+    },
+      header: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        alignItems: "center",
+        marginLeft: 100,
+      },
+      headerMain:{
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      name: {
+        color: "#4e32bc",
+        fontWeight: "900",
+        letterSpacing: 0.3,
+    },
+    settingRow: {
+        flexDirection: 'row',
+        justifyContent: 'left',
+        alignItems: 'center',
+        marginBottom: 20,
+      },
+      settingsTitle: {
+        fontSize: 20,
+        fontWeight: '500',
+        paddingLeft: 15,
+      },
+      settingLabel: {
+        fontSize: 16,
+      },
+      modalContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      modalContent: {
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+        width: '80%',
+        height: '60%', 
+      },
+      modalHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+      },
+      modalTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+      },
+      modalCloseButton: {
+        padding: 5,
+      },
+      modalCloseButtonText: {
+        fontSize: 16,
+        color: '#0066cc',
+      },
+      modalText: {
+        fontSize: 16,
+        marginBottom: 20,
+      },
+      notificationSettingRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+      },
+});
+
+export default styles;
